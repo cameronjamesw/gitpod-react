c61bdbf09db5ec5cd0b1cb8b11662fd580cedb48
Cam Wheatley <cameron_2000@live.co.uk>
2024-10-03 13:16:23 +0000

    add increment button, complete ternary expression lesson

--- my-app/src/components/StatefulGreeting.js
+++ my-app/src/components/StatefulGreeting.js
@@ -8,27 +8,36 @@ class StatefulGreeting extends React.Component {
         super(props);
         this.state = {
             introduction: "Welcome to the site!",
-            buttonText: "Exit"
+            buttonText: "Exit",
+            count: 0
         };
     }
 
     handleClick() {
     this.setState({
-        introduction: "Goodbye, thanks for visiting today!!",
-        buttonText: "Enter"
+        introduction: this.state.introduction === "Welcome to the site!" ? "Goodbye, thanks for visiting today!!" : "Welcome to the site!",
+        buttonText: this.state.buttonText === "Exit" ? "Enter" : "Exit"
     },
     () => {
         console.log("This is the updated state", this.state.introduction)
         console.log("This is the updated state", this.state.buttonText)
     })
-    console.log(this.state.introduction)
+    console.log(this.state.introduction);
+    }
+
+    incrementState() {
+        this.setState({
+            count: this.state.count+1
+        })
     }
 
     render() {
         return (
             <div>
                 <h1>{this.state.introduction} {this.props.greeting}</h1>
-                <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
+                <button onClick={() => this.handleClick()} > {this.state.buttonText}</button>
+                <button onClick={() => this.incrementState()}>Increment</button>
+                <p>You have clicked this button {this.state.count} times!</p>
             </div>
         )
     }
