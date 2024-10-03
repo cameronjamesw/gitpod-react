00f95a2b1c92cc169b34b85785a19eec705d9644
Cam Wheatley <cameron_2000@live.co.uk>
2024-10-03 15:12:51 +0000

    install es7, comment commands

---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,60 @@
+// rcfe
+
+// import React from 'react'
+
+// function ES7Snippets() {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+
+// rafce
+
+// import React from 'react'
+
+// const ES7Snippets = () => {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+
+// rce
+
+// import React, { Component } from 'react'
+
+// export class ES7Snippets extends Component {
+//   render() {
+//     return (
+//       <div>ES7Snippets</div>
+//     )
+//   }
+// }
+
+// export default ES7Snippets
+
+
+//rconst
+
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+//      first
+//   }
+// }
+
+
+//clg (console log)
+
+// console.log(first)
+
+
+// col (console log object)
+
+// console.log('first', first)
