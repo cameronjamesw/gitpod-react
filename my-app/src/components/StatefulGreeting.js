cef60058a115906e1b48c5cdf4cc81845b4b94cf
Cam Wheatley <cameron_2000@live.co.uk>
2024-10-03 12:25:48 +0000

    complete asynchronous function lesson

--- my-app/src/components/StatefulGreeting.js
+++ my-app/src/components/StatefulGreeting.js
@@ -1,8 +1,36 @@
 import React from 'react'
 
+
+
 class StatefulGreeting extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            introduction: "Welcome to the site!",
+            buttonText: "Exit"
+        };
+    }
+
+    handleClick() {
+    this.setState({
+        introduction: "Goodbye, thanks for visiting today!!",
+        buttonText: "Enter"
+    },
+    () => {
+        console.log("This is the updated state", this.state.introduction)
+        console.log("This is the updated state", this.state.buttonText)
+    })
+    console.log(this.state.introduction)
+    }
+
     render() {
-        return <h1>{this.props.greeting} Welcome back {this.props.name}, it's time to learn some more React!!</h1>
+        return (
+            <div>
+                <h1>{this.state.introduction} {this.props.greeting}</h1>
+                <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
+            </div>
+        )
     }
 };
 
